afb7463af3443ef33bd0283b1d1c5bb678901ff4
nickpwells <wells.nick53@gmail.com>
2015-05-12 20:48:37 -0700

    finished jquery, added list styling

--- js/app.js
+++ js/app.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
 
 	//Define checkmark
 	var xMark = '<i class="fa fa-times"></i>'
+	var count = 0;
 	
 	//Clears the placeholder when clicking the input
 	var listItem = document.getElementById("list-input");
 	$('#list-input').click(function(){
-		console.log('You clicked the input');
 		listItem.placeholder='';
 	});
 
@@ -23,29 +23,35 @@ $(document).ready(function(){
 	//Adds item to user's list
 	function addItem() {
 		var item = $('#list-input').val();
-		$('ul').append('<li class="list-item">'+item+xMark+'</li>');
+		$('ul').append('<li class="list-item">'+item+'</li>');
 		$('#list-input').val('');
 	}
 
 	postItem();
 
+	//xMark appears when user hovers over item
+	$('ul').on('mouseenter','.list-item',function(){
+		$(this).append(xMark);
+	}).on('mouseleave','.list-item',function(){
+		$(this).children().remove();
+	});
+
 	//allows user to cross and uncross items
 	$('ul').on('click','.list-item',function(){
-		if ($(this).hasClass('item-crossed')) {
-				console.log('you clicked again');
-				$(this).removeClass('item-crossed').css('background-color', '#edff8b');
-		}
-		else {
-			console.log('you clicked a list item');
-			$(this).addClass('item-crossed').css('background-color', '#808f3b');
-		}
-		}
-	); 
+		$(this).addClass('item-crossed').removeClass('list-item');
+	}); 
+
+	$('ul').on('click','.item-crossed',function(){
+		$(this).addClass('list-item').removeClass('item-crossed');
+	});
 
 	//removes item when user clicks on xMark
 	$('ul').on('click', '.list-item > i', function(e){
-		console.log('you clicked the xMark');
 		e.preventDefault();
-		$(this).parent().remove()
+		$(this).parent().remove();
 	});
+
+	
+	$('.list').append('<p class="count">'+'You currently have '+count+' items');
+
 });
